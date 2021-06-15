c3899b25753af69598a56a37c40df50bad414cdb
davidhuk <73908391+davidhuk@users.noreply.github.com>
2021-06-15 15:11:23 +0000

    Imported dnspython, Flask-PyMongo, pymongo, updated requirements.txt, configured database connection to MongoDB, created basic HTML template & tested data retrieval from database.

--- app.py
+++ app.py
@@ -1,5 +1,9 @@
 import os
-from flask import Flask
+from flask import (
+    Flask, flash, render_template,
+    redirect, request, session, url_for)
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
 if os.path.exists("env.py"):
     import env
 
@@ -7,9 +11,21 @@ if os.path.exists("env.py"):
 app = Flask(__name__)
 
 
+app.config["MONGO_DBNAME"] = os.environ.get("MONGO_DBNAME")
+app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
+app.secret_key = os.environ.get("SECRECT_KEY")
+
+
+mongo = PyMongo(app)
+
+
 @app.route("/")
-def hello():
-    return "This is an test message checking for Python3 & Flask."
+@app.route("/get_alcoholic_cocktails")
+def get_alcoholic_cocktails():
+    alcoholic_cocktails = list(mongo.db.alcoholic_cocktails.find())
+    return render_template(
+        "alcoholic_cocktails.html",
+        alcoholic_cocktails=alcoholic_cocktails)
 
 
 if __name__ == "__main__":
