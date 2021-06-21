acc0cdb41865a4271ca21feb1a8e9367f294f41a
davidhuk <73908391+davidhuk@users.noreply.github.com>
2021-06-21 13:40:29 +0000

    Installed Google Materialize navbar, mobile navbar and side navbar, updated navbar links to internal HTML. Modified contents of HTML templates. Updated custom CSS with Nunito, Google Fonts & text shadow. All changes tested and working.

--- app.py
+++ app.py
@@ -20,12 +20,23 @@ mongo = PyMongo(app)
 
 
 @app.route("/")
-@app.route("/get_alcoholic_cocktails")
+@app.route("/home_page")
+def get_home_page():
+    return render_template("home_page.html")
+
+
+@app.route("/alcoholic_cocktails")
 def get_alcoholic_cocktails():
     alcoholic_cocktails = list(mongo.db.alcoholic_cocktails.find())
-    return render_template(
-        "alcoholic_cocktails.html",
-        alcoholic_cocktails=alcoholic_cocktails)
+    return render_template("alcoholic_cocktails.html",
+                           alcoholic_cocktails=alcoholic_cocktails)
+
+
+@app.route("/non_alcoholic_cocktails")
+def get_non_alcoholic_cocktails():
+    non_alcoholic_cocktails = list(mongo.db.non_alcoholic_cocktails.find())
+    return render_template("non_alcoholic_cocktails.html",
+                           non_alcoholic_cocktails=non_alcoholic_cocktails)
 
 
 if __name__ == "__main__":
